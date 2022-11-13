d20ed322e5aa00388a0719d1ea563a30bc66e6ea
Can Çetin <108129872+cancetn@users.noreply.github.com>
2022-11-13 16:52:54 +0300

    Update Main.java

--- ebebek/primeNumber/src/Main.java
+++ ebebek/primeNumber/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         //Patika.dev ebebek Java & QA & SAP Practicum odevidir.
 
-        /* Java ile 1 - 100 arasındaki asal sayıları ekrana yazdıran programı yazınız.
+        /* Java ile 4 - 100 arasındaki asal sayıları ekrana yazdıran programı yazınız.
           Senaryo
           2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
         */
